63ff7b37ac1a2d64b42295add9c8cbda5aad183b
Nathan Clark <Nathan.Clark+github@tokenshift.com>
2015-07-16 15:52:12 -0400

    Skeleton and initial test case for parsing.

--- src/amy_six/user_agent.clj
+++ src/amy_six/user_agent.clj
@@ -1,5 +1,6 @@
 (ns amy-six.user-agent
-  "Helper functions for extracting information from User-Agent strings.")
+  "Helper functions for extracting information from User-Agent strings."
+  (:use [amy-six.matchers]))
 
 ;; Special thanks to http://useragentstring.com/ for listings of common
 ;; user agent strings and their breakdowns.
@@ -9,12 +10,13 @@
 
 (defn parse
   [user-agent]
-  {})
+  "Parse a User-Agent string into a map of things-we-know about the browser."
+  ; TODO: Heuristic for dealing with multiple, potentially conflicting, entries
+  ; for the same key.
+  (apply merge (matches user-agent matchers)))
 
-;(extend String
-;  UserAgent
-;  (family [this] (family (parse this))))
+(extend-type String UserAgent
+  (family [this] (family (parse this))))
 
-;(extend ILookup
-;  UserAgent
-;  (family [this] (:family this)))
+(extend-type clojure.lang.ILookup UserAgent
+  (family [this] (:family this)))
--- test/amy_six/user_agent/test.clj
+++ test/amy_six/user_agent/test.clj
@@ -1,3 +1,6 @@
 (ns amy-six.user-agent.test
   (:use amy-six.user-agent
         clojure.test))
+
+(deftest test-family
+  (is (= "Internet Explorer" (family "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; Media Center PC 6.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C)"))))
